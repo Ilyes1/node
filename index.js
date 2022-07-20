dd36f921e4242266c9270da012319aa6076d4d1b
Ilyes1 <ihannouch7@gmail.com>
2022-07-20 14:56:36 +0100

    make it better

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const axios = require('axios')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    async function getUniv() {
+        await axios.get('http://universities.hipolabs.com/search')
+        .then(data => res.render('index', {'items': data.data}))
+    }
+    getUniv() 
+})
+
+app.post('/country', (req, res) => {
+    const country = req.body.country
+
+    async function getUniv() {
+        await axios.get(`http://universities.hipolabs.com/search?country=${country}`)
+        .then(data => res.render('index', {'items': data.data}))
+    }
+    getUniv()
+})
+
+// app.get('/univ', (req, res) => {
+//     async function getUniv() {
+//         await axios.get('http://universities.hipolabs.com/search')
+//         .then(data => res.send(data.data))
+//     }
+//     getUniv() 
+// })
+
+app.listen(3000 || process.env.PORT)
